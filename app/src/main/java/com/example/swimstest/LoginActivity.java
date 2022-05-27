29b178f475202f8026506938c310b837112f458e
Jyotsna-Shetty <jyotssystems@gmail.com>
2022-05-27 20:35:49 +0530

    Add Barcode Scanner

---
+++ app/src/main/java/com/example/swimstest/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.swimstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
