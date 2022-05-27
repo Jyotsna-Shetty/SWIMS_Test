114b5637fad4db6b16291cf6d423f00aa7a27e84
Jyotsna-Shetty <jyotssystems@gmail.com>
2022-05-27 20:59:31 +0530

    Add Login Activity

--- app/src/main/java/com/example/swimstest/LoginActivity.java
+++ app/src/main/java/com/example/swimstest/LoginActivity.java
@@ -3,12 +3,27 @@ package com.example.swimstest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText emailText;
+    EditText passText;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailText.findViewById(R.id.EmailAddress);
+        passText.findViewById(R.id.TextPassword);
+        loginBtn.findViewById(R.id.LoginButton);
+
+        loginBtn.setOnClickListener(view -> {
+
+        });
+
     }
 }
