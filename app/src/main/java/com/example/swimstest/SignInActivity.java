6d8b068988d83add04afb04b392ae64c1fb7a4e2
Jyotsna-Shetty <jyotssystems@gmail.com>
2022-05-29 21:21:41 +0530

    Sign In Commit

--- app/src/main/java/com/example/swimstest/SignInActivity.java
+++ app/src/main/java/com/example/swimstest/SignInActivity.java
@@ -9,10 +9,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class SignInActivity extends AppCompatActivity {
@@ -20,6 +23,7 @@ public class SignInActivity extends AppCompatActivity {
     EditText passText;
     Button loginBtn;
     TextView tokenText;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,29 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     public void loginRequest() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JSONObject object = new JSONObject();
+        try {
+            //input your API parameters
+            object.put("email", emailText.getText().toString());
+            object.put("password", passText.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String url = "https://tools-management-dbms-project.herokuapp.com/api/account/signin/";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, object,
+                new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                tokenText.setText("String Response : "+ response.toString());
+                }
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tokenText.setText("Error getting response");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
 
     }
 }
